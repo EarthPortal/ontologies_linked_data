7daac0b0be57f19ecc02480e05e14d4d79e9e200
h.allem <h.allem@esi-sba.dz>
2025-04-09 15:02:39 +0200

    matching @context schema of project model

--- lib/ontologies_linked_data/models/project.rb
+++ lib/ontologies_linked_data/models/project.rb
@@ -1,42 +1,74 @@
 module LinkedData
   module Models
     class Project < LinkedData::Models::Base
-      model :project, :name_with => :acronym
+      model :project, :name_with => :acronym,
+            rdf_type: lambda { |*x| 'https://schema.org/ResearchProject' }
       
       def self.project_sources
         LinkedData.settings.connectors[:available_sources].keys
       end
 
       # Required attributes
-      attribute :acronym, enforce: [:unique, :existence]
-      attribute :creator, enforce: [:existence, :user, :list]
-      attribute :created, enforce: [:date_time], :default => lambda {|x| DateTime.now }
-      attribute :updated, enforce: [:date_time], :default => lambda {|x| DateTime.now }
-      attribute :type, enforce: [:existence], enforcedValues: %w[FundedProject NonFundedProject]
-      attribute :name, enforce: [:existence]
-      attribute :homePage, enforce: [:uri, :existence]
-      attribute :description, enforce: [:existence]
-      attribute :ontologyUsed, enforce: [:ontology, :list, :existence]
-      attribute :source, enforce: [:existence], enforcedValues: lambda { self.project_sources }
+      attribute :acronym, enforce: [:unique, :existence], 
+                namespace: :metadata, property: :acronym
+                
+      attribute :creator, enforce: [:existence, :user, :list], 
+                namespace: :schema, property: :creator
+                
+      attribute :created, enforce: [:date_time], :default => lambda {|x| DateTime.now },
+                namespace: :schema, property: :dateCreated
+                
+      attribute :updated, enforce: [:date_time], :default => lambda {|x| DateTime.now },
+                namespace: :schema, property: :dateModified
+                
+      attribute :type, enforce: [:existence], enforcedValues: %w[FundedProject NonFundedProject],
+                namespace: :metadata, property: :projectType
+                
+      attribute :name, enforce: [:existence],
+                namespace: :schema, property: :legalName
+                
+      attribute :homePage, enforce: [:uri, :existence],
+                namespace: :foaf, property: :homepage
+                
+      attribute :description, enforce: [:existence],
+                namespace: :schema, property: :description
+                
+      attribute :ontologyUsed, enforce: [:ontology, :list, :existence],
+                namespace: :metadata, property: :ontologyUsed
+                
+      attribute :source, enforce: [:existence], enforcedValues: lambda { self.project_sources },
+                namespace: :schema, property: :isBasedOn
       
       # Optional attributes
-      attribute :keywords, enforce: [:list]
-      attribute :contact, enforce: [:Agent]
-      attribute :organization, enforce: [:Agent]
-      attribute :logo, enforce: [:uri]
+      attribute :keywords, enforce: [:list],
+                namespace: :schema, property: :keywords
+                
+      attribute :contact, enforce: [:Agent],
+                namespace: :schema, property: :contactPoint
+                
+      attribute :organization, enforce: [:Agent],
+                namespace: :org, property: :memberOf
+                
+      attribute :logo, enforce: [:uri],
+                namespace: :schema, property: :logo
       
-      # Conditional attributes
-      attribute :grant_number, enforce: [:string]
-      attribute :start_date, enforce: [:date_time]
-      attribute :end_date, enforce: [:date_time]
-      attribute :funder, enforce: [:Agent]
-      
+      attribute :grant_number, enforce: [:string],
+                namespace: :schema, property: :identifier
+                
+      attribute :start_date, enforce: [:date_time],
+                namespace: :schema, property: :startDate
+                
+      attribute :end_date, enforce: [:date_time],
+                namespace: :schema, property: :endDate
+                
+      attribute :funder, enforce: [:Agent],
+                namespace: :schema, property: :funder
 
       embed :contact, :organization, :funder
       serialize_default :acronym, :type, :name, :homePage, :description, 
                       :ontologyUsed, :created, :updated, :keywords,
                       :contact, :organization, :grant_number, :start_date, 
-                      :end_date, :funder, :logo
+                      :end_date, :funder, :logo, :source
 
       write_access :creator
       access_control_load :creator
