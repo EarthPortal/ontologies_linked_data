123bed85317b82f8540bd4de0041e110a6e29e42
imene-amirat <imene2004.amirat@gmail.com>
2026-06-25 22:27:33 +0200

    fix: use metadata namespace for editWith

--- lib/ontologies_linked_data/models/ontology_submission.rb
+++ lib/ontologies_linked_data/models/ontology_submission.rb
@@ -113,7 +113,7 @@ module LinkedData
       attribute :competencyQuestion, namespace: :mod, type: :list
       attribute :wasGeneratedBy, namespace: :prov, type: :list
       attribute :wasInvalidatedBy, namespace: :prov, type: :list
-      attribute :editWith, namespace: :metadata_def, type: :uri
+      attribute :editWith, namespace: :metadata, type: :uri
 
       # Links
       attribute :pullLocation, type: :uri # URI for pulling ontology
