ebc92d1ae1f5bb0c4c8c9ace5feca54a49326a12
Imene Amirat <imene2004.amirat@gmail.com>
2026-07-23 15:02:38 +0200

    feat: replace toolName text with a real reference to the external tools model

--- lib/ontologies_linked_data/models/users/external_tool.rb
+++ lib/ontologies_linked_data/models/users/external_tool.rb
@@ -1,10 +1,11 @@
 module LinkedData::Models::Users
   class ExternalTool < LinkedData::Models::Base
     model :user_external_tool, name_with: lambda { |inst| uuid_uri_generator(inst) }
-    attribute :toolName, enforce: [:existence]
+    attribute :externalTool, enforce: [:existence, :external_tool]
     attribute :apikey, enforce: [:existence]
     attribute :user, inverse: {on: :user, attribute: :externalTools}
     embedded true
+    embed_values externalTool: [:name]
 
     attr_accessor :show_apikey
     serialize_never :show_apikey
