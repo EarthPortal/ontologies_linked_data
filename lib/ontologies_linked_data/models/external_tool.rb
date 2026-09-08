d8e4916de278a5f7a1f3141f61bf2ec90f9ede8f
Imene Amirat <imene2004.amirat@gmail.com>
2026-09-08 15:06:00 +0200

    add ExternalToolType model and update ExternalTool attributes

--- lib/ontologies_linked_data/models/external_tool.rb
+++ lib/ontologies_linked_data/models/external_tool.rb
@@ -1,13 +1,24 @@
 module LinkedData
   module Models
+    class ExternalToolType < LinkedData::Models::Base
+      VALUES = ["EDITOR", "VALIDATOR"]
+
+      model :external_tool_type, name_with: :type
+      attribute :type, enforce: [:unique, :existence]
+
+      enum VALUES
+    end
+
     class ExternalTool < LinkedData::Models::Base
       model :external_tool, name_with: :name
       attribute :name, enforce: [:unique, :existence]
       attribute :title, enforce: [:existence]
       attribute :homepage, enforce: [:existence, :uri]
+      attribute :externalToolType, enforce: [:existence, :external_tool_type]
       attribute :created, enforce: [:date_time], :default => lambda { |record| DateTime.now }
 
-      serialize_default :name, :title, :homepage, :created
+      serialize_default :name, :title, :homepage, :externalToolType, :created
+      embed_values externalToolType: [:type]
       cache_timeout 86400
     end
   end
